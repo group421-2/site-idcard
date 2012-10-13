eb1a795250f6e660faa66da9526aae280ed4674c
PeoneEr <PeoneErko@yandex.ru>
2012-10-13 12:25:06 +0700

    Rewrite write.class by Koala

--- www/app/core/write.class.php
+++ www/app/core/write.class.php
@@ -1,12 +1,27 @@
 <?php
 
-/**
- * TODO: реализовать класс
- */
 class write {
 
+    private $file_path;
+
     public function __construct() {
-        # code...
+        date_default_timezone_set('UTC');
+        $this->file_path = $_SERVER['DOCUMENT_ROOT'] . "/logs/" . date("d-m-Y") . ".log";
+        $handle = fopen($this->file_path, "x");
+        fclose($handle);
+    }
+
+    public function file_write($string) {
+        $handle = fopen($this->file_path, "a");
+        fwrite($handle, date("d.m.Y H:i" . "\n\r\n\r"));
+        fwrite($handle, $string . "\n\r\n\r\n\r");
+        fclose($handle);
+    }
+
+    public function file_read() {
+        /**
+         * Если понадобится,допишем
+         */
     }
 
 }
