4c159ab5b690e6fb3cabd988deb9ac84ae3434ab
Koala <karmickoala904@gmail.com>
2012-10-15 23:16:42 +0700

    css for 800 width

--- www/views/index/index.php
+++ www/views/index/index.php
@@ -6,7 +6,7 @@
         $('#scrollDown').cycle({
             sync: true,
             delay: -2000,
-            height: 700
+            height: 560
         });
     });
 </script>
@@ -32,7 +32,13 @@
         <img src="/files/photos/5.jpg" />
     </div>
     <div id="content">
-
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, 
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+        including versions of Lorem Ipsum.
     </div>
 
 </div>
