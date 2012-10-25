ab0b2707cf4b87cf816ffe293f94af20d4176ffb
PeoneEr <peoneerko@gmail.com>
2012-10-25 22:50:06 +0700

    finish gallery

--- www/views/add/js/gallery/album.js
+++ www/views/add/js/gallery/album.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
     $("a[rel=example_group]").fancybox({
-        prevEffect : 'fade',
-        nextEffect : 'fade',
         openEffect : 'elastic',
         closeEfftct :'elastic'
     });
