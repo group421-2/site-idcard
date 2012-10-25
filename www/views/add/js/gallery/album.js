ae0c94b5173bd520e344ff54b759b271f72fb7ed
PeoneEr <peoneerko@gmail.com>
2012-10-25 22:45:43 +0700

    update js libs

--- www/views/add/js/gallery/album.js
+++ www/views/add/js/gallery/album.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
     $("a[rel=example_group]").fancybox({
-        'transitionIn'		: 'none',
-        'transitionOut'		: 'none',
-        'titlePosition' 	: 'over',
-        'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
-            return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
-        }
+        prevEffect : 'fade',
+        nextEffect : 'fade',
+        openEffect : 'elastic',
+        closeEfftct :'elastic'
     });
 });
--- www/views/header.php
+++ www/views/header.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <script src="/views/add/js/libs/jquery-1.7.2.min.js"> </script>
-        <script type="text/javascript" src="/views/add/js/libs/jquery.mousewheel-3.0.4.pack.js"></script>
-        <script type="text/javascript" src="/views/add/js/libs/jquery.fancybox-1.3.4.pack.js"></script>
+        <script src="/views/add/js/libs/jquery-1.8.2.min.js"> </script>
+        <script type="text/javascript" src="/views/add/js/libs/jquery.mousewheel-3.0.6.pack.js"></script>
+        <script type="text/javascript" src="/views/add/js/libs/jquery.fancybox.pack.js"></script>
         <link rel="stylesheet" type="text/css" href="/views/add/css/index/index.css"></link>
         <link rel="stylesheet" type="text/css" href="/views/add/css/bootstrap.css"></link>
         <link rel="stylesheet" type="text/css" href="/views/add/css/bootstrap-responsive.css"></link>
-        <link rel="stylesheet" type="text/css" href="/views/add/css/jquery.fancybox-1.3.4.css"></link>
+        <link rel="stylesheet" type="text/css" href="/views/add/css/jquery.fancybox.css"></link>
 
     </head>
     <body>
